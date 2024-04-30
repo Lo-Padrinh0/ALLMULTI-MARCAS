40cf44af05bb24a70cc4d5dca3189bfd88879497
Lo-Padrinh0 <166052100+Lo-Padrinh0@users.noreply.github.com>
2024-04-30 17:49:36 -0300

    Anime prova

---
+++ Anime_Power/btnMenu.js
@@ -0,0 +1,9 @@
+function btnMenu() {
+    var btnMenu1 = document.getElementById('btnMenu1');
+
+    if (btnMenu1.style.display === 'block') {
+        btnMenu1.style.display = 'none';
+    } else {
+        btnMenu1.style.display = 'block';
+    }
+}
